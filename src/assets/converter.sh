5664f148edc1af614a3b026c7b8e13b2cd42fb3c
Adrien <adrien.bouteiller01@gmail.com>
2022-12-22 11:12:05 +0100

    up

--- src/assets/converter.sh
+++ src/assets/converter.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 for i in *.mkv;
-do (ffmpeg-bar -i "$i" -c:v libx264 -preset slow -crf 22 -vf subtitles="$i" "${i%.*}.mp4";
-rm "$i"
+do (
+# ffmpeg-bar -i "$i" -c:v libx264 -preset slow -crf 22 -vf subtitles="$i" "${i%.*}.mp4";
+# rm "$i"
+sshs -p "oNW98UkJb2E0btAG" scp $i u319375@u319375.your-storagebox.de:/
 );
 done
--- src/assets/verif.sh
+++ src/assets/verif.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 function all(){
-dirs=(/Users/a.bouteiller/Desktop/Adrien_perso/symfony-streaming/src/assets/*/)
+dirs=(/Users/a.bouteiller/Desktop/backend-streaming/src/assets/*/)
 for dir in "${dirs[@]}"
 
 do
+    echo "$dir"
     cd "$dir"
 function verif() {
     if [ "$(ls -A *.mkv)" ]; then
         echo "Il y a des fichiers mkv dans le dossier"
-        source /Users/a.bouteiller/Desktop/Adrien_perso/symfony-streaming/src/assets/converter.sh
+        source /Users/a.bouteiller/Desktop/backend-streaming/src/assets/converter.sh
         
     else
         echo "Il n'y a pas de fichiers mkv dans le dossier"
